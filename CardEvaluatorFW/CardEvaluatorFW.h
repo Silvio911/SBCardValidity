5c49e7f155a109ff1f060833d1889c21dedf4101
silvio <silvio.almotech@gmail.com>
2018-09-10 22:26:52 +0200

    Initial Commit

---
+++ CardEvaluatorFW/CardEvaluatorFW.h
@@ -0,0 +1,19 @@
+//
+//  CardEvaluatorFW.h
+//  CardEvaluatorFW
+//
+//  Created by Silvio Bulla on 10/09/2018.
+//  Copyright © 2018 Silvio Bulla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardEvaluatorFW.
+FOUNDATION_EXPORT double CardEvaluatorFWVersionNumber;
+
+//! Project version string for CardEvaluatorFW.
+FOUNDATION_EXPORT const unsigned char CardEvaluatorFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardEvaluatorFW/PublicHeader.h>
+
+
